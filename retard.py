361a34aa899029bac4a033237a1031028a0193f8
Judah Faison <150362509+Judah-Faison@users.noreply.github.com>
2025-09-18 09:33:49 -0400

    Add files via upload

--- retard.py
+++ retard.py
@@ -12,3 +12,31 @@ def djcows_doesnt_know_how_to_python(n): return ['retard'] * n
 
 def djcows_doesnt_know_how_to_python_but_worse(n): return ['retard' for _ in range(n)]
 # sidpatchy 2024-09-20
+
+# Judah-Faison 2025-9-18
+import os
+
+from ursina import Default, camera
+os.system("python import ursina")
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController as fpc
+
+app = Ursina()
+smart_dude=fpc()
+class Ground(Button):
+      def __init__(self, position = (0,0,0), color = color.white):
+         super().__init__(
+            parent = scene,
+            position = position,
+            model = 'cube',
+            origin_y = 0.5,
+            color = color,
+            scale = 0.5
+            
+            )
+
+Ground()
+
+Sky()
+app.run()
+# Judah-Faison 2025-9-18
